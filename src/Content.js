43d83189d77ef414c9da428aaa4fd7b4ef264830
Ho Thi Hoai <tomato.202404.hoth@gmail.com>
2024-06-11 16:42:38 +0900

    React Hook useEffect(callback, dependencies) --type3

--- src/Content.js
+++ src/Content.js
@@ -1,40 +1,74 @@
 import React, {useEffect, useState} from 'react'
 
+const tabs = ['posts', 'comments','albums'];
+
 function Content() {
-    // --For all useEffect types: Goi callback sau khi Component them element vao DOM
+    // --For all useEffect types: Goi callback sau khi Component them element vao DOM(component mount)
+    // --Note to cleanup function truoc khi unmount
 
     // 1. useEffect(callback)
     // --Goi callback moi khi component re-render
     // 2. useEffect(callback, [])
     // -- Goi callback mot lan sau khi component mount
     // 3. useEffect(ham callback, [dependencies])
-    const [title, setTitle] = useState('');
+    // -- Callback se duoc goi lai moi khi dependencies thay doi
+
+    const [selectedTab, setSelectedTab] = useState('posts');
     const [posts, setPosts] = useState([]);
+    const [showGoToTop, setShowGoToTop] = useState(false);
 
+    console.log(selectedTab)
     useEffect(() => {
-        //callback luon duoc goi sau khi component mount
-        document.title = title;
-
-        fetch('https://jsonplaceholder.typicode.com/posts')
+        fetch(`https://jsonplaceholder.typicode.com/${selectedTab}` )
             .then(res => res.json())
             .then(posts => {
                 setPosts(posts)
         })
-    }, []);
+    }, [selectedTab]);
 
+    useEffect(() => {
+        const handleScroll = () => {
+            setShowGoToTop(scrollY >= 200);
+            console.log(scrollY);
+        }
+        window.addEventListener('scroll', handleScroll) //luu y pham vi cua window
 
+        // clean up function
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        }
+    }, [])
 
+    console.log('Re-render')
     return (
         <div>
-            <input value={title} onChange={(event) => {
-               setTitle(event.target.value);
-            }} />
-            <ul>
-                {
-                    posts.map(post => {
-                    return <li key={post.id}>{post.title}</li>
-                })}
-            </ul>
+            {tabs.map(tab => (
+                <button
+                    key={tab}
+                    style = {tab === selectedTab ? {
+                        background: "black",
+                        color:'#FFFFFF'
+                    } : {}}
+                    onClick={() => setSelectedTab(tab)}
+                >
+                    {tab}
+                </button>
+            ))}
+            {
+                posts.map(post => {
+                return <li key={post.id}>{post.title || post.name}</li>
+            })}
+            {showGoToTop && (
+                <button
+                    style={{
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20
+                    }}
+                >
+                    Go to top
+                </button>)
+            }
         </div>
     )
 }
