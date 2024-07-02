3afc56a0ea207ff34a0aeb974f7e72ddead593e1
Ho Thi Hoai <tomato.202404.hoth@gmail.com>
2024-07-02 16:28:22 +0900

    Delete unused file

--- src/ThemeContext.js
+++ src/ThemeContext.js
@@ -1,6 +1,7 @@
 import React, {createContext, useState} from "react";
 
-
+//To transfer data directly between components, no middle object
+// component A => B => C => when cutting B(middle object => we need to fix code to transfer data from A=> C)
 
 export const ThemeContext = createContext();
 
--- src/redux/useContextHook.js
+++
@@ -1,7 +0,0 @@
-import {useContext} from "react";
-
-//To transfer data directly between components, no middle object
-// component A => B => C => when cutting B(middle object => we need to fix code to transfer data from A=> C)
-
-
-
